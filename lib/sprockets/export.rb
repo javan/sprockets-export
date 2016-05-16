528e76d8ccfc83f690865703aa6ca7129f6654e7
Javan Makhmali <javan@javan.us>
2016-05-16 18:12:48 -0400

    Expire cache when version changes

--- lib/sprockets/export.rb
+++ lib/sprockets/export.rb
@@ -1,8 +1,11 @@
+require "pathname"
+
 require "sprockets/export/version"
 require "sprockets/export/namespace"
 require "sprockets/export/directive_processor"
 require "sprockets/export/metadata_injector"
 require "sprockets/export/bundle_processor"
 
+Sprockets.append_path Pathname.new(__dir__).join("..")
 Sprockets.register_postprocessor    "application/javascript", Sprockets::Export::MetadataInjector
 Sprockets.register_bundle_processor "application/javascript", Sprockets::Export::BundleProcessor
--- lib/sprockets/export/directive_processor.rb
+++ lib/sprockets/export/directive_processor.rb
@@ -2,6 +2,7 @@ class Sprockets::DirectiveProcessor
   def process_export_directive(namespace)
     if @content_type == "application/javascript"
       Sprockets::Export::Namespace.save(@environment, @filename, namespace)
+      process_depend_on_directive "sprockets/export/version.rb"
     end
   end
 end
